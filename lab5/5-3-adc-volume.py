f0dc01581061c3fb903eae738ea1e8ce9e1b68bb
SimonovAleksey <aleksej33simonov@gmail.com>
2025-05-16 10:35:31 +0300

    Create 5-3-adc-volume.py

---
+++ lab5/5-3-adc-volume.py
@@ -0,0 +1,79 @@
+import RPi.GPIO as gpio
+from time import sleep
+gpio.setmode(gpio.BCM)
+dac=[8, 11, 7, 1, 0, 5, 12, 6]
+comp=14
+troyka=13
+gpio.setup(dac, gpio.OUT)
+gpio.setup(troyka,gpio.OUT, initial=gpio.HIGH)
+gpio.setup(comp, gpio.IN)
+
+def decimal2bin(a):
+    return [int (elem) for elem in bin(a)[2:].zfill(8)]
+
+def adc():
+    k=0
+    for i in range(7, -1, -1):
+        k+=2**i
+        gpio.output(dac, decimal2bin(k))
+        time.sleep(0.001)
+        if gpio.input(comp)==0:
+            k-=2**i
+    return k
+def adc1():
+    value = 128
+    gpio.output(dac, decimal2bin(value))
+    if gpio.input(comp) == 0:
+        value -= 64
+    else:
+        value += 64
+    gpio.output(dac, decimal2bin(value))
+    if gpio.input(comp) == 0:
+        value -= 32
+    else:
+        value += 32
+    gpio.output(dac, decimal2bin(value))
+
+    if gpio.input(comp) == 0:
+        value -= 16
+    else:
+        value += 16
+    gpio.output(dac, decimal2bin(value))
+    if gpio.input(comp) == 0:
+        value -= 8
+    else:
+        value += 8
+    gpio.output(dac, decimal2bin(value))
+    if gpio.input(comp) == 0:
+        value -= 4
+    else:
+        value += 4
+    gpio.output(dac, decimal2bin(value))
+    if gpio.input(comp) == 0:
+        value -= 2
+    else:
+        value += 2
+    gpio.output(dac, decimal2bin(value))
+    if gpio.input(comp) == 0:
+        value -= 1
+    else:
+        value += 1
+    gpio.output(dac, decimal2bin(value))
+    return value
+
+def volume(n):
+    n = int(n/256*10)
+    mass = [0]*8
+    for i in range(n-1):
+        mass[i]=1
+    return mass
+
+try:
+    while True:
+        i = adc1()
+        if i!=0:
+            gpio.output(leds, volume(i))
+            print(int(i/256*10))
+finally:
+    gpio.output(dac, 0)
+    gpio.cleanup()    
