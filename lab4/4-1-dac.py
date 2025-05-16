494c3e1ad21ba363bb452f9f54172bf501e19e32
SimonovAleksey <aleksej33simonov@gmail.com>
2025-05-16 11:17:36 +0300

    Update 4-1-dac.py

--- lab4/4-1-dac.py
+++ lab4/4-1-dac.py
@@ -1,28 +1,27 @@
 import RPi.GPIO as GPIO
-dac=[8, 11, 7, 1, 0, 5, 12, 6]
+dac = [8, 11, 7, 1, 0, 5, 12, 6]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(dac, GPIO.OUT)
-def decimal2bin(a):
-    return [int (elem) for elem in bin(a)[2:].zfill(8)]
+def bintrans(s):
+    return [int(b) for b in bin(s)[2:].zfill(8)]
 try:
     while True:
-        number = input('Введите число от 0 до 255')
-        if number == "q":
-            exit()
+        a = input()
+        if a == 'q':
+            break
         try:
-            if int(number) > 255:
-                print("Число должно быть до 255!")
-            elif int(number) < 0:
-                print("Число должно быть положительным!")
+            n = int(a)
+            if not 0 <= n <= 255:
+                print("Число должно быть от 0 до 255!")
+                continue
+            bins = bintrans(n)
+            GPIO.output(dac, bins)
+            U = (n/255)*3.3
+            print(f"Напряжение: {U:.2f} Вольт")
         except ValueError:
-            try: 
-                if float(number):
-                    print("Число не должно быть типа float!")  
-            except ValueError:
-                print("Вы ввели строку... Введите число!")
-        else:
-            GPIO.output(dac, decimal2bin(int(number)))
-            print("{:.4f}".format(int(number)/256*3.3))
+            print("Значение должно быть целым числом!")
+except KeyboardInterrupt:
+    print("0")
 finally:
     GPIO.output(dac, 0)
     GPIO.cleanup()
